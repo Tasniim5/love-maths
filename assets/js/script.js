fe05f887eeb2ccd21bc38d189c90958ede5d4849
Tasniim A <tas.ali05@hotmail.co.uk>
2024-01-26 14:26:01 +0000

    added js functions

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function runGame(){};
+function check Answer(){};
+function calculateCorrectAnswer(){};
+function incrementScore (){};
+function incrementWrongAnswer(){};
+function displayAdditionQuestion(){};
+function displaySubtractionQuestion(){};
+function displayMultiplyQuestion(){};
+function displayDivideQuestion(){};
