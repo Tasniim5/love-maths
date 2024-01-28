c6e853a4ec2b294bfb60c77357094a9d9f2c9f77
Tasniim A <tas.ali05@hotmail.co.uk>
2024-01-28 20:14:31 +0000

    division added

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ document.getElementById("answer-box").addEventListener("keydown", function (even
     if(event.key === "Enter"){
         checkAnswer();
     }
-})
+});
     runGame("addition");
 
 });
@@ -45,6 +45,11 @@ document.getElementById("answer-box").focus();
     else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
     } 
+
+    else if (gameType === "division" ) {
+        displayDivisionQuestion(num1, num2);
+    } 
+
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,6 +94,10 @@ function calculateCorrectAnswer() {
     }
       else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+      }
+
+      else if (operator === "/") {
+        return [operand1 / operand2, "division"];
       }
 
     else {
@@ -138,4 +147,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
+   
+    
 }
